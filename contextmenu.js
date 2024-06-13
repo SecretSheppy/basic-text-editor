3cfecd6a80b9d6200062ea63fba8c3f57b33e264
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2024-06-13 17:15:30 +0100

    refactor: refactored context menu

---
+++ contextmenu.js
@@ -0,0 +1,108 @@
+(function () {
+    'use strict';
+
+    const CONTEXT_MENU_X_OFFSET = 150;
+    const CONTEXT_MENU_Y_OFFSET = 100;
+    const KEYBINDINGS_MENU_OFFSET = 200;
+
+    let contextMenu = document.getElementById('context-menu');
+    let toggleTerminalButton = document.getElementById('cm-toggle-terminal');
+    let saveButton = document.getElementById('cm-save');
+    let newButton = document.getElementById('cm-new');
+    let keyBindingsButton = document.getElementById('cm-key-bindings');
+    let quitButton = document.getElementById('cm-quit');
+    let keyBindingsMenu = document.getElementById('key-bindings');
+
+    /**
+     * Shows the context menu at the correct position. Most of the time this
+     * will be at the specified x and y coordinates, but if the context menu
+     * would be off-screen, its position be adjusted using the
+     * {@link CONTEXT_MENU_X_OFFSET} and {@link CONTEXT_MENU_Y_OFFSET}
+     * constants.
+     *
+     * @param {number} x The x coordinate of the mouse event.
+     * @param {number} y The y coordinate of the mouse event.
+     */
+    function showContextMenu(x, y) {
+        contextMenu.style.display = 'flex';
+
+        if (y > window.innerHeight - CONTEXT_MENU_Y_OFFSET) {
+            y -= CONTEXT_MENU_Y_OFFSET;
+        }
+        contextMenu.style.top = `${y}px`;
+
+        if (x > window.innerWidth - CONTEXT_MENU_X_OFFSET) {
+            x -= CONTEXT_MENU_X_OFFSET;
+        }
+        contextMenu.style.left = `${x}px`;
+    }
+
+    /**
+     * Hides the context menu.
+     */
+    function hideContextMenu() {
+        contextMenu.style.display = 'none';
+    }
+
+    /**
+     * Shows the key-bindings menu.
+     */
+    function showKeyBindings() {
+        keyBindingsMenu.style.display = 'flex';
+    }
+
+    /**
+     * Hides the key-bindings menu.
+     */
+    function hideKeyBindings() {
+        keyBindingsMenu.style.display = 'none';
+    }
+
+    /**
+     * Adds event listeners to the document and the context menu buttons.
+     */
+    function addListeners () {
+
+        document.addEventListener('contextmenu', function (event) {
+            event.preventDefault();
+            showContextMenu(event.clientX, event.clientY);
+        });
+
+        document.addEventListener('click', function () {
+            hideContextMenu();
+        });
+
+        keyBindingsButton.addEventListener('mouseover', function (event) {
+            if (event.clientX < window.innerWidth - KEYBINDINGS_MENU_OFFSET) {
+                showKeyBindings();
+            }
+        });
+
+        keyBindingsButton.addEventListener('mouseout', function () {
+            hideKeyBindings();
+        });
+
+        toggleTerminalButton.addEventListener('click', function () {
+            toggleTerminal();
+        });
+
+        saveButton.addEventListener('click', function () {
+            if (cwf !== '') {
+                fs.writeFileSync(cwf, document.getElementById('editor-text').value, 'utf8');
+                writeTerminalLine(`File saved: ${cwf}`);
+                hideNotSavedIndicator();
+            }
+        });
+
+        newButton.addEventListener('click', function () {
+            newDocument();
+        });
+
+        quitButton.addEventListener('click', function () {
+            nw.App.quit();
+        });
+
+    }
+
+    module.exports = exports = { addListeners };
+})();
