15283a7bb7568c0ed2e74511848b13489a440eb2
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2024-06-16 12:28:11 +0100

    docs: added documentation to the files module

--- files.js
+++ files.js
@@ -7,10 +7,28 @@
     const terminal = require('./terminal.js');
     const editor = require('./editor.js');
 
+    /**
+     * Takes a path that could be composed of any number of parent directory
+     * references including with directories on top of those and then sanitizes
+     * it using {@link path.resolve} and then ensuring that the Windows
+     * backslash directory system is replaced with forward slashes to resemble
+     * the Windows terminal and be uniform with the linux and mac experience.
+     *
+     * @param {string} fullPath The full string path composed of directories
+     * and parent directory references.
+     * @returns {string} The sanitized path without parent directory references.
+     */
     function normalizePath(fullPath) {
         return path.resolve(fullPath).replaceAll('\\', '/');
     }
 
+    /**
+     * Scans the specified directory for all files and folders and then
+     * displays them in the terminal. Directories and displayed in bold and
+     * a different color that is specified by the selected theme.
+     *
+     * @param {string} path The full string path.
+     */
     function scanDirectory(path) {
         fs.readdirSync(path).forEach(function (file) {
             if (fs.lstatSync(path + '/' + file).isDirectory()) {
@@ -21,21 +39,40 @@
         });
     }
 
-    function changeDirectory(newPath) {
-        let tempDir = environment.cwd + '/' + newPath;
+    /**
+     * Executed when the cd command is used to change the current directory.
+     * Used to determine whether the directory exists. Will set the value of
+     * {@link environment.cwd} to the new directory if it exists, if not it
+     * will log to the console. There are two cases where an error can occur,
+     * if the directory does not exist or if the user is treating a file
+     * as a directory.
+     *
+     * @param {string} directory This single value of directory that should be
+     * concatenated with the current value of {@link environment.cwd}
+     */
+    function changeDirectory(directory) {
+        let tempDir = environment.cwd + '/' + directory;
 
         try {
             if (fs.lstatSync(tempDir).isDirectory()) {
                 environment.cwd = normalizePath(tempDir);
                 terminal.setCwd(environment.cwd);
             } else {
-                terminal.writeLine(`Not a directory: ${newPath}`);
+                terminal.writeLine(`Not a directory: ${directory}`);
             }
         } catch (e) {
-            terminal.writeLine(`Directory not found: ${newPath}`);
+            terminal.writeLine(`Directory not found: ${directory}`);
         }
     }
 
+    /**
+     * Opens a file into the editor window.
+     *
+     * @param {string} file The path to the file. Once a file has been opened
+     * its path will be stored in {@link environment.cwf} and ctrl + s
+     * (unless default binding is changed) can be used to automatically save
+     * the file.
+     */
     function openFile(file) {
         try {
             editor.setText(fs.readFileSync(environment.cwd + '/' + file, 'utf8'));
@@ -46,6 +83,12 @@
         }
     }
 
+    /**
+     * Sets {@link environment.cwf} path then saves the current text in the
+     * editor to the file in at the {@link environment.cwf} path.
+     *
+     * @param {string} fileName The name of the file to save.
+     */
     function saveFile(fileName) {
         let fullPath = environment.cwd + '/' + fileName;
 
@@ -59,6 +102,15 @@
         }
     }
 
+    /**
+     * Deletes the specified file by concatenating the file name with the value
+     * of {@link environment.cwf} and then unlinking the path from the file
+     * tree. If the file does not exist, an error will be logged to the
+     * application terminal.
+     *
+     * @param {string} fileName The name and extension of the file that should
+     * be deleted in the current working directory.
+     */
     function removeFile(fileName) {
         let fullPath = environment.cwd + '/' + fileName;
 
@@ -71,6 +123,12 @@
         }
     }
 
+    /**
+     * Creates a directory with the specified name in the current working
+     * directory.
+     *
+     * @param {string} dirName The name of the directory to create.
+     */
     function makeDirectory(dirName) {
         let fullPath = environment.cwd + '/' + dirName;
 
@@ -82,6 +140,13 @@
         }
     }
 
+    /**
+     * Removes a directory with the given name from the current working
+     * directory. If the directory does not exist an error will be written
+     * to the application console.
+     *
+     * @param {string} dirName the name of the directory to delete.
+     */
     function removeDirectory(dirName) {
         let fullPath = environment.cwd + '/' + dirName;
 
